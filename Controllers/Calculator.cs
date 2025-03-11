c6c2a42a185d64e22e045afa2472ef44ee71797e
Shiva <shivabhaskarm3@gmail.com>
2025-03-10 22:39:10 -0400

    Initial Coding

---
+++ Controllers/Calculator.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Calculator : ControllerBase
+    {
+        [HttpGet]
+        [Route("AddNumbers")]
+        public async Task<IActionResult> AddNumbers(decimal? firstNumber, decimal? secondNumber)
+        {
+            if (firstNumber != null && secondNumber != null)
+            {
+                return Ok(await Task.FromResult(firstNumber + secondNumber));
+            }
+            else
+            {
+                return BadRequest("Both numbers must be provided.");
+            }
+        }
+
+        [HttpGet]
+        [Route("SubtractNumbers")]
+        public async Task<IActionResult> SubtractNumbers(decimal? firstNumber, decimal? secondNumber)
+        {
+            if (firstNumber != null && secondNumber != null)
+            {
+                return Ok(await Task.FromResult(firstNumber - secondNumber));
+            }
+            else
+            {
+                return BadRequest("Both numbers must be provided.");
+            }
+        }
+
+        [HttpGet]
+        [Route("MultiplyNumbers")]
+        public async Task<IActionResult> MultiplyNumbers(decimal? firstNumber, decimal? secondNumber)
+        {
+            if (firstNumber != null && secondNumber != null)
+            {
+                return Ok(await Task.FromResult(firstNumber * secondNumber));
+            }
+            else
+            {
+                return BadRequest("Both numbers must be provided.");
+            }
+        }
+
+        [HttpGet]
+        [Route("DivideNumbers")]
+        public async Task<IActionResult> DivideNumbers(decimal? firstNumber, decimal? secondNumber)
+        {
+            if (firstNumber != null && secondNumber != null)
+            {
+                if (secondNumber != 0)
+                {
+                    return Ok(await Task.FromResult(firstNumber / secondNumber));
+                }
+                else
+                {
+                    return BadRequest("Second number should not be zero when dividing");
+                }
+                
+            }
+            else
+            {
+                return BadRequest("Both numbers must be provided.");
+            }
+        }
+    }
+}
